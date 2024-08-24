89bd76cf33b7534b603cb125e4eb6b139688d6ff
Premnishok-Muhilan <premnishok4@gmail.com>
2024-08-24 08:50:56 +0530

    Used useLoaderData in Home.jsx and mapped the todo data as a list item

--- loaders/todoLoader.jsx
+++ loaders/todoLoader.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 const todoLoader = async () => {
   //make a request to the server
   const response = await axios.get("http://localhost:3001/api/v1/todos");
-  console.log(response.data);
-  return response.data;
+
+  return response.data.todos;
 };
 
 export default todoLoader;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+
 const Home = () => {
+
+  const todos = useLoaderData();
+
   return (
-    <div>
-      <h1>Hello World!</h1>
+      <div>
+      <h1>Todos</h1>
+      <ul>
+        {
+          todos.map(todo => (
+            <li key={todo._id}>
+              {todo.description}
+            </li>
+          ))
+        }
+      </ul>
     </div>
-  );
-};
+  )
+}
 
 export default Home;
