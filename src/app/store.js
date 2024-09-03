2bd971b4fd51604510487378875e6aa563f50d9b
Premnishok-Muhilan <premnishok4@gmail.com>
2024-09-03 20:46:50 +0530

    register page flow completed

--- src/app/store.js
+++ src/app/store.js
@@ -1,23 +1,12 @@
-// Import the `configureStore` function from `@reduxjs/toolkit`.
-// This function is used to create and configure a Redux store with various settings and middleware.
 import { configureStore } from "@reduxjs/toolkit";
-
-// Import the `todoReducer` from the `../features/todos/todoSlice` module.
-// This reducer is responsible for handling actions and updating the state related to todos.
-//importing the default export from the ../features/todos/todoSlice module.
-//This default export is expected to be a Redux reducer function that manages the state related to todos.
 import todoReducer from "../features/todos/todoSlice";
+import registerReducer from "../features/users/registerSlice";
 
-// Create the Redux store using `configureStore`.
-// This store configuration includes the reducer and any additional settings or middleware.
 const store = configureStore({
-  // Define the reducers for the store.
-  // In this case, we have a single reducer for managing the `todos` state.
-  reducer: {
-    todos: todoReducer,
-  },
+    reducer: {
+        todos: todoReducer,
+        register: registerReducer
+    }
 });
 
-// Export the configured store as the default export from this module.
-// This allows the store to be imported and used in the application, typically in the root component.
 export default store;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,34 +1,77 @@
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom";
+import { selectEmail, selectName, selectPassword, setEmail, setName, setPassword } from "../features/users/registerSlice";
+import userServices from "../services/userServices";
+
 const Register = () => {
-    return (
-        <div className="container mt-5">
-            <div className="row">
-                  <div className="col-md-6 offset-md-3">
-                      <div className="card">
-                          <div className="card-header">
-                              <h4>Register</h4>
-                          </div>
-                          <div className="card-body">
-                              <form>
-                                  <div className="mb-3">
-                                      <label htmlFor="name" className="form-label">Name</label>
-                                      <input type="text" className="form-control" id="name" />
-                                  </div>
-                                  <div className="mb-3">
-                                      <label htmlFor="email" className="form-label">Email</label>
-                                      <input type="email" className="form-control" id="email" />
-                                  </div>
-                                  <div className="mb-3">
-                                      <label htmlFor="password" className="form-label">Password</label>
-                                      <input type="password" className="form-control" id="password" />
-                                  </div>
-                                  <button type="submit" className="btn btn-primary">Register</button>
-                              </form>
-                          </div>
-                      </div>
-                </div>
-            </div>
+
+    const name = useSelector(selectName);
+    const email = useSelector(selectEmail);
+    const password = useSelector(selectPassword);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        
+        userServices.register({ name, username: email, password })
+            .then(response => {
+                alert(response.data.message);
+                
+                // clear the form
+                dispatch(setName(''));
+                dispatch(setEmail(''));
+                dispatch(setPassword(''));
+
+                setTimeout(() => {
+                    navigate('/login');
+                }, 500);
+            })
+            .catch(error => {
+                alert(error.response.data.message);
+            });
+    }
+
+  return (
+      <div className="container mt-5">
+          <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <div className="card">
+                        <div className="card-header">
+                            <h4>Register</h4>
+                        </div>
+                        <div className="card-body">
+                            <form onSubmit={handleRegister}>
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="form-label">Name</label>
+                                  <input type="text" className="form-control" id="name" 
+                                      value={name}
+                                      onChange={e => dispatch(setName(e.target.value))}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">Email</label>
+                                  <input type="email" className="form-control" id="email" 
+                                        value={email}
+                                        onChange={e => dispatch(setEmail(e.target.value))}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                  <input type="password" className="form-control" id="password" 
+                                        value={password}
+                                        onChange={e => dispatch(setPassword(e.target.value))}
+                                    />
+                                </div>
+                                <button type="submit" className="btn btn-primary">Register</button>
+                            </form>
+                        </div>
+                    </div>
+              </div>
           </div>
-    )
-  }
-  
-  export default Register
+        </div>
+  )
+}
+
+export default Register
