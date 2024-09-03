3c4b2524de02d45b7a4d88996d2a44934538b38f
Premnishok-Muhilan <premnishok4@gmail.com>
2024-09-03 20:19:42 +0530

    added bootstrap + moved the todos to dashboard as parent route + home page with register & login

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,12 @@
-// Import the `createRoot` function from the `react-dom/client` module.
-// This function is used to create a root for rendering a React application in the DOM.
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { Provider } from 'react-redux';
+import store from './app/store.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
-// Import the `App` component from the `./App.jsx` file.
-// This component will serve as the main component of the React application.
-import App from "./App.jsx";
-
-// Import the `Provider` component from the `react-redux` module.
-// This component is used to make the Redux store available to the React application.
-import { Provider } from "react-redux";
-
-// Import the `store` from the `./app/store.js` file.
-// The `store` is the Redux store that holds the application's state.
-import store from "./app/store.js";
-
-// Create a root for rendering the React application by targeting the DOM element with the id "root".
-// Use the `createRoot` function from the `react-dom/client` module to do this.
-createRoot(document.getElementById("root")).render(
-  // Render the React application inside the root.
-  // Wrap the `App` component with the `Provider` to make the Redux store available to the entire component tree.
-  <Provider store={store}>
-    {/* Render the `App` component as the main component of the application */}
-    <App />
-  </Provider>
+createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
